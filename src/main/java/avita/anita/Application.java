4be48a88be1008b71065e3bb27d47b765db7029c
abhijit <abhijitbeherame@gmail.com>
2019-12-09 23:41:43 +0530

    abhi

---
+++ src/main/java/avita/anita/Application.java
@@ -0,0 +1,20 @@
+package avita.anita;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import top.lshaci.framework.mybatis.MybatisApplication;
+import top.lshaci.framework.swagger.SwaggerApplication;
+
+@SpringBootApplication
+@Import({MybatisApplication.class, SwaggerApplication.class})
+@MapperScan("avita.anita.mapper")
+public class Application {
+
+	public static void main(String[] args)  {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
